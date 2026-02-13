1ccce49f5a041e30bf9abc15a7bee109ac5654d2
José Andres Garcia <jogarciab07@gmail.com>
2026-02-13 13:23:04 -0500

    Is Leap Year

--- LogicConcepts/IsLeeapYear/Program.cs
+++ LogicConcepts/IsLeeapYear/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using Shared;
+
+do
+{
+    var year = ConsoleExtension.GetInt("Ingrese año: ");
+
+    if (year > 0)
+    {
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+        {
+            Console.WriteLine($"El año: {year}, SI es bisiesto");
+        }
+        else
+        {
+            Console.WriteLine($"El año: {year}, NO es bisiesto");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Ingrese un año válido, este es un año imposible de ejecutar");
+    }
+}
+while (true);
