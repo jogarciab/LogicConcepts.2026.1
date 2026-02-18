43333515f62fb790f470065fe189d0c13c9a33d6
José Andres Garcia <jogarciab07@gmail.com>
2026-02-18 09:55:27 -0500

    Soltion exercice 12

--- LogicConcepts/NumbersSeries/Program.cs
+++ LogicConcepts/NumbersSeries/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    var n = ConsoleExtension.GetInt("Cuantos números desea: ");
+    int sum = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        Console.Write($"{i}\t");
+        sum += i;
+    }
+    var average = sum / n;
+    Console.WriteLine();
+    Console.WriteLine($"La suma es....: {sum,20:N0}");
+    Console.WriteLine($"El promedio es: {average,2:N2}");
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]i, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+Console.WriteLine("Game Over");
