32c223f02581ebcb7b35591567a182ef54c13618
José Andres Garcia <jogarciab07@gmail.com>
2026-02-18 12:39:55 -0500

    Solution exercice 13

---
+++ LogicConcepts/Factorial/Program.cs
@@ -0,0 +1,20 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    var fact = ConsoleExtension.GetInt("Ingrese un número: ");
+    var factorial = MyMath.Factorial(fact);
+    Console.WriteLine($"{fact}! = {factorial:N0}");
+
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]i, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+Console.WriteLine("Game Over");
---
+++ Shared/MyMath.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared
+{
+    public class MyMath
+    {
+        public static double Factorial(int n)
+        {
+            double factorial = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+    }
+}
