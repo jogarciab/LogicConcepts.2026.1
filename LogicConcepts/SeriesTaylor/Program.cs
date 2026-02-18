8844fa27a5a1b49bf4604034a8502bb47cf105bb
José Andres Garcia <jogarciab07@gmail.com>
2026-02-18 13:00:52 -0500

    Solution exercice 14

---
+++ LogicConcepts/SeriesTaylor/Program.cs
@@ -0,0 +1,31 @@
+﻿using Shared;
+using System.Net;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    var n = ConsoleExtension.GetInt("Cuantos términos desea: ");
+    var x = ConsoleExtension.GetDouble("Digita el valor de x..: ");
+    var taylor = Taylor(x, n);
+    Console.WriteLine($"f ({x}) = {taylor:N6}");
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]i, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+double Taylor(double x, int n)
+{
+    double sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += Math.Pow(x, i) / MyMath.Factorial(i);
+    }
+    return sum;
+}
+
+Console.WriteLine("Game Over");
